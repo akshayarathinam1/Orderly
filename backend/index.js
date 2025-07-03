a1df3dde5fc89e30b806e23ddd3a1043c07911f8
Akshaya R <148430911+akshayarathinam1@users.noreply.github.com>
2025-07-03 17:38:59 +0530

    done backend

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+
+// App config 
+const app = express();
+const port = 4000;
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("API Working");
+});
+
+// Listener
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
